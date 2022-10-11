18a528df52922200c4b7b057f9985276eb99660b
Aleksandr-Abramov <ale19850505@gmail.com>
2022-10-11 14:57:53 +0300

    feature: сверстал header1280/768/320

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './header/header'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Ghbdtn</h2>
-    </div>
+    <Header />
   );
 }
 
