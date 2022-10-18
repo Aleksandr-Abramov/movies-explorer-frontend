7f30912f873ef9c08c1d98adbaadc685a43360af
Aleksandr-Abramov <ale19850505@gmail.com>
2022-10-18 21:36:46 +0300

    feature: сверстал register-page1280/768/320-1.0

--- src/components/register-page/register/Register.jsx
+++ src/components/register-page/register/Register.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import './register.css';
-// import profilePhoto1280 from '../../../../images/png/home-page/photo-1280.png';
-// import profilePhoto768 from '../../../../images/png/home-page/photo-768.png';
-// import profilePhoto320 from '../../../../images/png/home-page/photo-320.png';
+import Input from '../../shared/input/Input';
+import logo from '../../../images/svg/home-page/logo.svg';
 
 export default function Register() {
   return (
     <main className='register'>
-    
-        <h1>register</h1>
+    <div className='register__content-container'>
+      <a href='/' className='register__logo-link'>
+        <img className='register__logo-img' src={logo} alt='logo' />
+      </a>
+      <h1 className='register__title'>Добро пожаловать!</h1>
+      <form action='/' method='post' className='register__form'>
+        <Input type='text' name='name' id='name' labelName='Имя' />
+        <Input type='email' name='email' id='email' labelName='E-mail' />
+        <Input
+          type='password'
+          name='password'
+          id='password'
+          labelName='Пароль'
+          minlength={2}
+        />
+        <button type='submit' className='register__form-btn'>
+          Зарегистрироваться
+        </button>
+      </form>
+      <div className='register__link-container'>
+        <span className='register__span'>Уже зарегистрированы?</span>
+        <a href='/' className='register__link'>
+          Выход
+        </a>
+      </div>
+      </div>
     </main>
   );
 }
