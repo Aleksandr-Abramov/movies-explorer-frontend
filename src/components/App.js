e2ccd25c85a7259ea5ff9a513edca8d42921a45b
Aleksandr-Abramov <ale19850505@gmail.com>
2022-10-19 21:44:05 +0300

    feature: сверстал 404-page1280/768/320-1.0

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import './App.css';
 // import Footer from './home-page/footer/Footer.jsx'
 // import Main  from './main-page/main-tag/Main-home-page';
 // import Register  from './register-page/register/Register';
-import Login  from './login-page/login/Login';
-import Profile from './profile-page/profile/Profile';
+// import Login  from './login-page/login/Login';
+// import Profile from './profile-page/profile/Profile';
+import NotFound404 from './page-404/page-404/NotFound404';
 // import AboutTheProject  from '../components/main-page/aboutTheProject/AboutTheProject';
 // import Technologies  from '../components/main-page/technologies/Technologies';
 // import Profile  from '../components/main-page/profile/Profile';
@@ -20,7 +21,8 @@ function App() {
     <Footer /> */}
     {/* <Register /> */}
     {/* <Login /> */}
-    <Profile />
+    {/* <Profile /> */}
+    <NotFound404 />
   </>
  );
    
---
+++ src/components/page-404/page-404/NotFound404.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './notFound.css';
+
+
+export default function NotFound404() {
+  return (
+
+      <main className='not-found'>
+        <div className="not-found__content-container">
+            <h1 className='not-found__title'>404</h1>
+            <p className='not-found__text'>Страница не найдена</p>
+        </div>
+        <a href="/" className='not-found__link'>Назад</a>
+      </main>
+
+  );
+}
