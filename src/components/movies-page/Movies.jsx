cc273889c4db0801a70a1d7e81cbb6d0ac38054b
Aleksandr-Abramov <ale19850505@gmail.com>
2022-10-21 18:48:20 +0300

    feature: сверстал preloader1280/768/320-1.0

--- src/components/movies-page/Movies.jsx
+++ src/components/movies-page/Movies.jsx
@@ -4,6 +4,7 @@ import Footer from '../shared/footer/Footer'
 import Header from '../shared/header/Header';
 import SearchForm from './search-form/SearchForm';
 import MainMenuAuthorized from '../shared/main-menu-authorized/MainMenuAuthorized';
+import Preloader from './preloader/Preloader';
 
 export default function Movies() {
   return (
@@ -15,8 +16,9 @@ export default function Movies() {
     <main className='movies'>
     <div className="wrapper">
     <SearchForm />
+    <Preloader />
+
     </div>
-    
     </main> 
     <Footer/>
     </>
