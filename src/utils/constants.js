1359ba3cfc0fa6d8850f368aed73f6aefdafa3eb
Aleksandr-Abramov <alexAbramov1985@yandex.ru>
2022-11-27 19:45:18 +0000

    feature: прохождение ревью0.3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ export const BEATIFULMOVIESURL = {
   url: 'https://api.nomoreparties.co/beatfilm-movies',
 };
 export const APIMAINURL = {
-  url: 'http://localhost:3000',
+ // url: 'http://localhost:3000',
+  url: 'https://api.movies-alex.nomoredomains.icu',
 };
 
